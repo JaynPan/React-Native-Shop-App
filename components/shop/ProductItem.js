e93569b64932498af58ed5999303cf62529f7d2d
Jay Pan <lb0104672@gmail.com>
2020-07-19 12:38:09 +0800

    Create mockup of all products page

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  View, Text, Image, StyleSheet, Button,
+} from 'react-native';
+
+import Colors from '../../constants/Colors';
+
+const styles = StyleSheet.create({
+  product: {
+    shadowColor: '#000',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 5, // for andriod
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    height: 300,
+    margin: 20,
+  },
+  imageContainer: {
+    width: '100%',
+    height: '60%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+
+  },
+  info: {
+    height: '100%',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  details: {
+    alignItems: 'center',
+    height: '15%',
+    padding: 10,
+  },
+  title: {
+    fontSize: 18,
+    marginVertical: 4,
+  },
+  price: {
+    fontSize: 14,
+    color: '#888',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '25%',
+  },
+});
+
+export default function ProductItem({
+  image, title, price, onViewDetail, onAddCart,
+}) {
+  return (
+    <View style={styles.product}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ url: image }} />
+      </View>
+      <View style={styles.info}>
+        <View style={styles.detailes}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.price}>{price.toFixed(2)}</Text>
+        </View>
+        <View style={styles.actions}>
+          <Button color={Colors.accent} title="View Details" onPress={onViewDetail} />
+          <Button color={Colors.accent} title="To Cart" onPress={onAddCart} />
+        </View>
+      </View>
+    </View>
+  );
+}
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = () => {
   const products = useSelector((state) => state.products.availableProducts);
+
+  const onViewDetail = () => {};
+
+  const onAddCart = () => {};
+
   return (
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={({ item }) => (
+        <ProductItem
+          image={item.imageUrl}
+          title={item.title}
+          price={item.price}
+          onViewDetail={onViewDetail}
+          onAddCart={onAddCart}
+        />
+      )}
     />
   );
 };
